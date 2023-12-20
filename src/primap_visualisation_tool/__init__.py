93386d578e8353f8680f94e2f3d476fb8e6f4848
Daniel Busch <danielbusch@climate-resource.com>
2023-12-20 15:44:46 +0100

    fix docstring and gitigore

--- src/primap_visualisation_tool/__init__.py
+++ src/primap_visualisation_tool/__init__.py
@@ -1,5 +1,7 @@
 """
-This app visualises GHG-emissions over time based on sectors, countries and entities. It is based on the Primap dataset.
+App for visualising GHG-emissions over time based on sectors, countries and entities.
+
+It is based on the Primap dataset.
 """
 import importlib.metadata
 
