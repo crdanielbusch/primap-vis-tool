160e9f8a382fdbffc93f4f9684a0a8b1f0220300
Daniel Busch <danielbusch@climate-resource.com>
2023-12-21 14:30:18 +0100

    data reading

---
+++ src/primap_visualisation_tool/app.py
@@ -0,0 +1,22 @@
+"""
+Launch plotly app.
+
+Author: Daniel Busch, Date: 21-12-2023
+"""
+
+from pathlib import Path
+
+import primap2 as pm  # type: ignore
+
+#  define folders
+print("Reading data set")
+root_folder = Path(__file__).parent.parent.parent
+data_folder = Path("data")
+primaphist_data_folder = Path("data") / "PRIMAP-hist_data"
+
+#  data reading
+current_version = "v2.5_final"
+old_version = "v2.4.2_final"
+combined_ds = pm.open_dataset(
+    root_folder / data_folder / f"combined_data_{current_version}_{old_version}.nc"
+)
