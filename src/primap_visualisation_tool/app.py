3339d706790944ca5c96b45ec965e88744b86a75
Daniel Busch <danielbusch@climate-resource.com>
2024-02-06 14:52:13 +0100

    add changes from main

--- src/primap_visualisation_tool/app.py
+++ src/primap_visualisation_tool/app.py
@@ -9,6 +9,7 @@ import csv
 from collections.abc import Sized
 from datetime import datetime
 from pathlib import Path
+from typing import Any
 
 import dash_ag_grid as dag  # type: ignore
 import dash_bootstrap_components as dbc  # type: ignore
@@ -21,7 +22,7 @@ import xarray as xr
 from attrs import define
 from dash import Dash, Input, Output, State, callback, ctx, dcc, html  # type: ignore
 
-from primap_visualisation_tool.definitions import SUBENTITIES, index_cols
+from primap_visualisation_tool.definitions import LINES_LAYOUT, SUBENTITIES, index_cols
 from primap_visualisation_tool.functions import apply_gwp, select_cat_children
 
 
@@ -87,6 +88,9 @@ class AppState:  # type: ignore
 
     source_scenario_index: int
     """Index of the current source-scenario"""
+
+    source_scenario_visible: dict[str, bool]
+    """Selected source scenarios in the overview plot legend"""
 
     ds: xr.Dataset
     """Dataset to plot from"""
@@ -187,6 +191,32 @@ class AppState:  # type: ignore
             return
 
         self.source_scenario_options = tuple(new_source_scenario_options)
+
+    # TODO: remove based on discussion here
+    # https://github.com/crdanielbusch/primap-vis-tool/pull/22/files#r1474566618
+    def update_source_scenario_visible(
+        self,
+        legend_value: list[Any],  # TODO if possible, give more detailed type hint
+        figure_data: dict[str, Any],
+    ) -> None:
+        """
+        Update the selected lines in the overview plot legend.
+
+        Parameters
+        ----------
+        legend_value
+            The legend value that was clicked and triggered the callback.
+        figure_data
+            The overview plot figure.
+        """
+        lines_in_figure = [i["name"] for i in figure_data["data"]]
+
+        lines_to_change = [lines_in_figure[i] for i in legend_value[1]]
+
+        for source_scenario, new_value in zip(
+            lines_to_change, legend_value[0]["visible"]
+        ):
+            self.source_scenario_visible[source_scenario] = new_value
 
     def update_all_indexes(
         self, country: str, category: str, entity: str, source_scenario: str
@@ -340,6 +370,12 @@ class AppState:  # type: ignore
         fig = go.Figure()
 
         for source_scenario in self.source_scenario_options:
+            # check if layout is defined
+            if source_scenario in LINES_LAYOUT:
+                line_layout = LINES_LAYOUT[source_scenario]
+            else:
+                line_layout = {}  # empty dict creates random layout
+
             df_source_scenario = filtered_pandas.loc[
                 filtered_pandas["SourceScen"] == source_scenario
             ]
@@ -349,6 +385,8 @@ class AppState:  # type: ignore
                     y=list(df_source_scenario[self.entity]),
                     mode="lines",
                     name=source_scenario,
+                    line=line_layout,
+                    visible=self.source_scenario_visible[source_scenario],
                 )
             )
 
@@ -605,6 +643,13 @@ def get_default_app_starting_state(
 
     source_scenario_options = tuple(combined_ds["SourceScen"].to_numpy())
 
+    source_scenario_visible = {
+        k: v
+        for (k, v) in zip(
+            source_scenario_options, [True] * len(source_scenario_options)
+        )
+    }
+
     app_state = AppState(
         country_options=country_dropdown_options,
         country_name_iso_mapping=country_name_iso_mapping,
@@ -617,6 +662,7 @@ def get_default_app_starting_state(
         source_scenario_index=source_scenario_options.index(
             start_values["source_scenario"]
         ),
+        source_scenario_visible=source_scenario_visible,
         ds=combined_ds,
         filename=filename,
     )
@@ -1053,6 +1099,35 @@ def update_entity_graph(  # noqa: PLR0913
 
 
 @callback(  # type: ignore
+    Output("memory_visible_lines", "data"),
+    Input("graph-overview", "restyleData"),
+    State("graph-overview", "figure"),
+    prevent_initial_call=True,
+)
+def update_visible_lines_dict(
+    legend_value: list[Any],
+    figure_data: dict[str, Any],
+    app_state: AppState | None = None,
+) -> None:
+    """
+    Update which lines are selected in the legend of overview plot.
+
+    Parameters
+    ----------
+    legend_value
+        Information about which line was clicked in legend
+    figure_data
+        The overview plot
+    app_state
+        Application state. If not provided, we use `APP_STATE` from the global namespace.
+    """
+    if app_state is None:
+        app_state = APP_STATE
+
+    app_state.update_source_scenario_visible(legend_value, figure_data)
+
+
+@callback(  # type: ignore
     Output(
         "note-saved-div",
         "children",
@@ -1116,6 +1191,10 @@ if __name__ == "__main__":
                         dbc.Stack(
                             [
                                 dcc.Store(id="memory"),  # invisible
+                                dcc.Store(
+                                    id="memory_visible_lines",
+                                    data=APP_STATE.source_scenario_visible,
+                                ),
                                 html.B(
                                     children="Country",
                                     style={"textAlign": "left", "fontSize": 14},
