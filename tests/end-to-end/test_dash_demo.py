08fffc8c5eabe188356381380b158f23d7b7693e
Daniel Busch <danielbusch@climate-resource.com>
2024-03-12 11:50:05 +0100

    selenium version

--- tests/end-to-end/test_dash_demo.py
+++ tests/end-to-end/test_dash_demo.py
@@ -15,6 +15,7 @@ def test_001_child_with_0(dash_duo):
     # 5. use wait_for_* if your target element is the result of a callback,
     # keep in mind even the initial rendering can trigger callbacks
     dash_duo.wait_for_text_to_equal("#nully-wrapper", "0", timeout=4)
+
     # 6. use this form if its present is expected at the action point
     assert dash_duo.find_element("#nully-wrapper").text == "0"
     # 7. to make the checkpoint more readable, you can describe the
