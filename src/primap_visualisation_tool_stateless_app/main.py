3480e1b6eeb6322d78e24dcaf52520b0de421c0a
Zebedee Nicholls <zebedee.nicholls@climate-resource.com>
2024-05-13 10:05:58 +0200

    Change default place for database

--- src/primap_visualisation_tool_stateless_app/main.py
+++ src/primap_visualisation_tool_stateless_app/main.py
@@ -27,8 +27,13 @@ from primap_visualisation_tool_stateless_app.dataset_holder import (
 @click.option(
     "--notes-db",
     required=False,
-    default=Path("default-notes-db.db"),
-    help="Database file for notes",
+    default=None,
+    help=(
+        "Database file for notes "
+        "(if not supplied, we will save into a file in the same directory as the dataset,"
+        "with the same name as dataset, "
+        "with the extension changed to `.db`)"
+    ),
     type=click.Path(file_okay=True, dir_okay=False, readable=True, writable=True),
 )
 @click.option(
@@ -36,12 +41,16 @@ from primap_visualisation_tool_stateless_app.dataset_holder import (
     help="Should we run in debug mode",
     default=True,
 )
-def run_app(port: int, dataset: str, notes_db: str, debug: bool) -> None:
+def run_app(port: int, dataset: str, notes_db: str | None, debug: bool) -> None:
     """
     Run the PRIMAP visualisation tool
     """
+    dataset = Path(dataset)
     loaded_ds = pm.open_dataset(dataset)
     set_application_dataset(loaded_ds)
+
+    if notes_db is None:
+        notes_db = dataset.with_suffix(".db")
 
     primap_visualisation_tool_stateless_app.notes.db_filepath_holder.APPLICATION_NOTES_DB_PATH_HOLDER = Path(
         notes_db
