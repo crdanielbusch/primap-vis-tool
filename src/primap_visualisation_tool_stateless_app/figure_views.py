2fbaa4af394d170b004eff8a1c6acde665ec457d
Daniel Busch <danielbusch@climate-resource.com>
2024-07-25 10:11:55 +0200

    range slider works again

--- src/primap_visualisation_tool_stateless_app/figure_views.py
+++ src/primap_visualisation_tool_stateless_app/figure_views.py
@@ -12,17 +12,22 @@ def update_xy_range(xyrange: dict[str, Any], figure: Any) -> Any:
     TODO: write
     """
     logger.info(f"{xyrange=}")
-    for axis in ["xaxis", "yaxis"]:
-        if xyrange[axis] == "autorange":
-            update_value = {axis: {"autorange": True}}
-        else:
-            update_value = {axis: {"range": xyrange[axis], "autorange": False}}
 
-        # Hmmm, really don't like these if statements, one for another day
-        if isinstance(figure, dict):
-            figure["layout"].update(**update_value)
-
-        else:
-            figure.update_layout(**update_value)
-
+    # Hmmm, really don't like these if statements, one for another day
+    if isinstance(figure, dict):
+        # figure["layout"].update(**update_value)
+        for axis in ["xaxis", "yaxis"]:
+            if xyrange[axis] == "autorange":
+                # update_value = {axis : {"autorange" : True}}
+                figure["layout"][axis]["autorange"] = True
+            else:
+                # update_value = {axis : {"range" : xyrange[axis], "autorange" : False}}
+                figure["layout"][axis]["range"] = xyrange[axis]
+                figure["layout"][axis]["autorange"] = False
+    else:
+        pass
+        # figure.update_layout(**update_value)
+    print("Updated figure")
+    print(f"{figure['layout']['xaxis']=}")
+    print(f"{figure['layout']['yaxis']=}")
     return figure
