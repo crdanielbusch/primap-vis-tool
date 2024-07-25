52742155f459cd9f0efd3f7a760a5fea33af1013
Daniel Busch <danielbusch@climate-resource.com>
2024-07-25 17:38:53 +0200

    clean up

--- src/primap_visualisation_tool_stateless_app/figure_views.py
+++ src/primap_visualisation_tool_stateless_app/figure_views.py
@@ -24,16 +24,14 @@ def update_xy_range(xyrange: dict[str, Any], figure: Any) -> Any:
 
     # Hmmm, really don't like these if statements, one for another day
     if isinstance(figure, dict):
-        # figure["layout"].update(**update_value)
         for axis in ["xaxis", "yaxis"]:
             if xyrange[axis] == "autorange":
                 # There may be a smarter way to update the values of the dict
-                # without removing 'title' and 'type'
                 figure["layout"][axis]["autorange"] = True
             else:
                 figure["layout"][axis]["range"] = xyrange[axis]
                 figure["layout"][axis]["autorange"] = False
     else:
         print("Figure not a dict")
-        # figure.update_layout(**update_value)
+
     return figure
